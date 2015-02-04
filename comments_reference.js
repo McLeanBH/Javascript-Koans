a834879af78f97d906aa09d75e42f7ecbec5093f
McLeanBH <mcleanbh@gmail.com>
2015-02-04 09:49:16 -0500

    exersices begun - yet to finish

---
+++ comments_reference.js
@@ -0,0 +1,20 @@
+
+
+// In AboutObjects.js ---> lines 36-37
+
+// AboutFxns.js ---> lines 55-56
+
+// existing comment about potential mistake in AboutArrays.js line 6
+
+//
+// (personal notes)
+// 11pm on Tuesday, I am ~ half way through the exercises, finished all sections of AboutFxns
+// Will resume tomorrow, applying necessary changes to 'HigherOrderFxns' 'Inheritance' & 'Mutability'
+// - once I can actually think straight again. Unable to retain all that I am leanring at the moment, and this is without a doubt (if not the most) one of the most beneficial assignments provided thus far.
+// why are we not given such an assignment early on? I think that it would make much more sense than just throwing us into applications in which we do not understand the functionality and power of javascript.
+//
+// ****DONT FORGET *********
+// update (unposted) blog. Have not made an update since the middle of last week, and much as occurred since then, including
+// working in our first group assignment, building the chat app, learning new terminal applications and methods such as gitX and zsh
+// - discuss how the first month of the course has unfolded, perform a personal SWOT analysis of my skills and areas that need improvement
+//
