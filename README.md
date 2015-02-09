77c78b7b66af407fe4240af65965d3b72abf2f3e
McLeanBH <mcleanbh@gmail.com>
2015-02-09 16:02:52 -0500

    commiting updates and notes of what to be further included soon

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
+Please see notes in 'comments_refernece.js' as well as @ the top of 'About Applying What We have Learnt' file.
+
+I plan to do these, at minimum, 2 more times all the way through, and to utilize all the appropriate & necessary steps as outlined.
+Want to work all the way through the problems, from A - to Z (beginning to end) finding most efficient way of doing them coupled with my best understanding. 
+
+
 # Javascript-Koans
 
 # Javascript Koans - koans to learn Javascript (the good bits) #
 
-Based on Edgecase's fantastic 
+Based on Edgecase's fantastic
 [Ruby koans](http://github.com/edgecase/ruby_koans), the goal of the
 Javascript koans is to teach you Javascript programming through
 testing.
@@ -20,7 +26,7 @@ you to solve more complicated problems and use more advanced techniques.
 ### Running the Koans from a Browser
 
 Simply navigate to the Javascript Koans folder using a file browser, and
-double click on KoansRunnner.html. 
+double click on KoansRunnner.html.
 
 Any browser will do, but for the best results Firefox or Chrome is
 recommended. More stack trace information shows up for javascript on these
@@ -31,7 +37,3 @@ refresh the browser. Rinse and repeat until all tests turn green.
 
 The test runner used is [Jasmine](http://pivotal.github.com/jasmine/) with a
 customized report viewer.
-
-
-
-
--- comments_reference.js
+++ comments_reference.js
@@ -1,4 +1,11 @@
+// monday February 9th. (note to Jake)
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// I am closing the issue now, but I still have not had the opportunity to work through these like I would like to & as I plan to.
+// just trying to get caught up to speed in regards to my HW submissions, so that we may discuss during our 1-on-1 for my performance assessment.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // In AboutObjects.js ---> lines 36-37
 
@@ -11,7 +18,7 @@
 // 11pm on Tuesday, I am ~ half way through the exercises, finished all sections of AboutFxns
 // Will resume tomorrow, applying necessary changes to 'HigherOrderFxns' 'Inheritance' & 'Mutability'
 // - once I can actually think straight again. Unable to retain all that I am leanring at the moment, and this is without a doubt (if not the most) one of the most beneficial assignments provided thus far.
-// why are we not given such an assignment early on? I think that it would make much more sense than just throwing us into applications in which we do not understand the functionality and power of javascript.
+//
 //
 // ****DONT FORGET *********
 // update (unposted) blog. Have not made an update since the middle of last week, and much as occurred since then, including
